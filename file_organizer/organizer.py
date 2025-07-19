252dbc7abf2206818fee56412c8fb3e34942276b
mandel <nelsonokiki@gmail.com>
2025-07-19 18:21:00 +0100

    Initial commit: Set up basic structure for file organizer

---
+++ file_organizer/organizer.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+# Define file types and their corresponding folders
+file_types = {
+    '.jpg': 'Images',
+    '.png': 'Images',
+    '.docx': 'Documents',
+    '.pdf': 'Documents'
+}
